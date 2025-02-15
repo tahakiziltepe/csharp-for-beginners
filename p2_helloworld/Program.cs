3b13ecafbd0601316db67a1e45561cf7117f4c74
tahakiziltepe <tahakiziltepe@hotmail.com>
2025-02-16 02:02:12 +0300

    fix

--- p2_helloworld/Program.cs
+++ p2_helloworld/Program.cs
@@ -1,3 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+// Part 1
 Console.WriteLine("Hello, World!");
+
+// Part 2
+string firstFriend = "Maria";
+string secondFriend = "Sage";
+Console.WriteLine($"My friends are {firstFriend} and {secondFriend}");
+
+// Part 3
+string firstFriend3 = "  Maria  ";
+firstFriend = firstFriend3.TrimStart();
+
+string secondFriend3 = "   Sage ";
+secondFriend = secondFriend3.TrimEnd();
+
+string thirdFriend3 = "  Johnson  ";
+thirdFriend3 = thirdFriend3.Trim();
+
+Console.WriteLine($"My friends are {firstFriend3} and {secondFriend3} and {thirdFriend3}");
--- p3_workingWithStrings/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-string firstFriend = "Maria";
-string secondFriend = "Sage";
-Console.WriteLine($"My friends are {firstFriend} and {secondFriend}");
