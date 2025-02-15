803de6d4705ff3685f597c578be726a189fc050d
tahakiziltepe <tahakiziltepe@hotmail.com>
2025-02-16 01:51:07 +0300

    p3

---
+++ p3_workingWithStrings/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string firstFriend = "Maria";
+string secondFriend = "Sage";
+Console.WriteLine($"My friends are {firstFriend} and {secondFriend}");
