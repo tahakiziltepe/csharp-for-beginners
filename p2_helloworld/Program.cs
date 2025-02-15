f1bb064d8eba7fad47b4668356d8b30ca731bbe9
tahakiziltepe <tahakiziltepe@hotmail.com>
2025-02-16 02:15:00 +0300

    p6

--- p2_helloworld/Program.cs
+++ p2_helloworld/Program.cs
@@ -3,19 +3,31 @@
 // Part 1
 Console.WriteLine("Hello, World!");
 
-// Part 2
+// Part 3
 string firstFriend = "Maria";
 string secondFriend = "Sage";
 Console.WriteLine($"My friends are {firstFriend} and {secondFriend}");
 
-// Part 3
-string firstFriend3 = "  Maria  ";
-firstFriend = firstFriend3.TrimStart();
+// Part 5
+string firstFriend5 = "  Maria  ";
+firstFriend5 = firstFriend5.TrimStart();
 
-string secondFriend3 = "   Sage ";
-secondFriend = secondFriend3.TrimEnd();
+string secondFriend5 = "   Sage ";
+secondFriend5 = secondFriend5.TrimEnd();
 
-string thirdFriend3 = "  Johnson  ";
-thirdFriend3 = thirdFriend3.Trim();
+string thirdFriend5 = "  Johnson  ";
+thirdFriend5 = thirdFriend5.Trim();
 
-Console.WriteLine($"My friends are {firstFriend3} and {secondFriend3} and {thirdFriend3}");
+Console.WriteLine($"My friends are {firstFriend5} and {secondFriend5} and {thirdFriend5}");
+
+// Part 6
+string friends = $"My friends are {firstFriend5} and {secondFriend5} and {thirdFriend5}";
+Console.WriteLine(friends.Replace("Sage", "Hank"));
+string newFriends = friends.Replace("Maria", "John");
+Console.WriteLine(newFriends);
+
+Console.WriteLine(newFriends.Contains("Maria"));
+Console.WriteLine(newFriends.ToUpper());
+Console.WriteLine(newFriends.Length);
+Console.WriteLine(newFriends.StartsWith("My"));
+Console.WriteLine(newFriends.EndsWith("Maria"));
